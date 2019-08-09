a0304cf388d861190fbcbc9dfe6d135e4800c2a4
Guilherme Beidaki Peres <guilherme.peres@tecnospeed.com.br>
2019-08-09 13:42:52 -0300

    app ok

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const fs = require('fs')
 const path = require('path')
 const ytdl = require('ytdl-core')
 
-const URL = 'https://www.youtube.com/watch?v=BaDrwNYuaOE'
-const VIDEO_NAME = 'aula4.mp4'
+const URL = 'VIDEO_URL'
+const VIDEO_NAME = 'VIDEO_NAME.mp4'
 
 console.log('Fetching video...')
 
-ytdl(URL)
-  .pipe(fs.createWriteStream(path.resolve(__dirname, '..', `videos/${VIDEO_NAME}`)))
+ytdl(URL).pipe(
+  fs.createWriteStream(
+    path.resolve(__dirname, `videos/${VIDEO_NAME}`)
+  )
+)
